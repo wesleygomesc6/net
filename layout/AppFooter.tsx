9cd56546231209e614665532481aaf9f474d113d
Wesley Gomes <wesleygomesc6@gmail.com>
2026-04-20 09:21:49 -0300

    configurando primereact para utlizar como modelo o template primesakai

---
+++ layout/AppFooter.tsx
@@ -0,0 +1,14 @@
+
+
+
+const AppFooter = () => {
+
+    return (
+        <div className="layout-footer">
+            
+            <span className="font-medium ml-2"> &copy; Wesley Gomes</span>
+        </div>
+    );
+};
+
+export default AppFooter;
---
+++ layout/AppMenu.tsx
@@ -0,0 +1,44 @@
+ 
+
+import { AppMenuItem } from '@/types';
+import AppMenuitem from './AppMenuitem';
+import { MenuProvider } from './context/menucontext';
+
+const AppMenu = () => {
+
+    const model: AppMenuItem[] = [
+        {
+            label: 'Home',
+            items: [{ label: 'Dashboards', icon: 'pi pi-fw pi-home', to: '/' }]
+        },
+        {
+            label: 'Gestão',
+            items: [
+                { label: 'Alunos', icon: 'pi pi-fw pi-id-card', to: '/alunos' },
+                { label: 'Disciplinas', icon: 'pi pi-fw pi-check-square', to: '/disciplinas' },
+                { label: 'Professores', icon: 'pi pi-fw pi-bookmark', to: '/professores' },
+                { label: 'Turmas', icon: 'pi pi-fw pi-exclamation-circle', to: '/turmas' }
+                
+            ]
+        },
+        {
+            label: 'Discentes',
+            items: [
+                { label: 'Turmas', icon: 'pi pi-fw pi-eye', to: '/turmas'}
+            ]
+        },
+        
+    ];
+
+    return (
+        <MenuProvider>
+            <ul className="layout-menu">
+                {model.map((item, i) => {
+                    return !item?.seperator ? <AppMenuitem item={item} root={true} index={i} key={item.label} /> : <li className="menu-separator"></li>;
+                })}
+            </ul>
+        </MenuProvider>
+    );
+};
+
+export default AppMenu;
