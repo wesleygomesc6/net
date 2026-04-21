3d4d33eb43c37058ac7104a3cdb5876abbd16311
Wesley Gomes <wesleygomesc6@gmail.com>
2026-04-21 17:14:30 -0300

    adiciona tabela de alunos com carregamento e cálculo de idade

--- app/alunos/page.tsx
+++ app/alunos/page.tsx
@@ -2,18 +2,26 @@
 import { AlunoProps } from "@/types/app/aluno";
 import { useEffect, useState } from "react";
 import { listarAlunos } from "../api/aluno/listar";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { calcularIdade } from "../utils/calcular-idade";
 
 export default function AlunosPage() {
     const [alunos, setAlunos] = useState<AlunoProps[]>([]);
+    const [loadingAlunos, setLoadingAlunos] = useState(false);
  
     useEffect(() => {
         async function fetchAlunos() {
+            setLoadingAlunos(true);
             try {
                 const response = await listarAlunos();
                 setAlunos(response);
             } catch (error) {
                 console.error("Erro ao listar alunos:", error);
+            }finally {
+                setLoadingAlunos(false);
             }
+
         }
         fetchAlunos();
     }, []);
@@ -21,11 +29,24 @@ export default function AlunosPage() {
   return (
     <div>
       <h1>Alunos</h1>
-      <ul>
-        {alunos.map((aluno) => (
-          <li key={aluno.id}>{aluno.nome}</li>
-        ))}
-      </ul>
+      <DataTable
+        value={alunos}
+        loading={loadingAlunos}
+        stripedRows
+        rowHover
+      >
+        <Column field="id" header="Matrícula" />
+        <Column field="nome" header="Nome" />
+        <Column field="email" header="E-mail" />
+        <Column field="idade" header="Idade" body={(rowData) => {
+            const { anos, meses } = calcularIdade(rowData.nascimento);
+            return `${anos} anos e ${meses} meses`;
+        }} />
+        <Column field="turmas" header="Turmas" body={(rowData) => {
+            return rowData.turmas?.map((turma) => turma.nome).join(', ') || 'Sem turmas';
+        }} />
+      </DataTable>
+      
     </div>
   );
 }
